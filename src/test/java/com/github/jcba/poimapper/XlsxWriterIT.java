8d13e9af9dc4322e05dbd4ad1114634b42279d2f
Job Bakker <job190@gmail.com>
2024-04-21 21:10:51 +0200

    feat: add simple test which writes to file

---
+++ src/test/java/com/github/jcba/poimapper/XlsxWriterIT.java
@@ -0,0 +1,40 @@
+package com.github.jcba.poimapper;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class XlsxWriterIT {
+
+    @Test
+    void writesToFile_givenAnnotatedClass() throws IOException {
+        var testRows = List.of(
+                new ColumnAnnotatedAndFormatted("val1", 1.0111, "test"),
+                new ColumnAnnotatedAndFormatted("val4", 2.123113412, "test")
+        );
+
+        var workbook = new XSSFWorkbook();
+        var sheet = workbook.createSheet("test");
+        new XlsxSheetWriter<>(ColumnAnnotatedAndFormatted.class, workbook, sheet).write(testRows.stream());
+
+        var out = new FileOutputStream("/tmp/demo.xlsx");
+        workbook.write(out);
+        out.close();
+
+        assertThat(Path.of("/tmp/demo.xlsx")).exists();
+    }
+
+    record ColumnAnnotatedAndFormatted(
+            @Column String first,
+            @Column @ColumnFormat(value = "#.####", type = CellType.NUMERIC) Double second,
+            String notWritten
+    ) {
+    }
+}
--- src/test/java/com/github/jcba/poimapper/XlsxWriterTest.java
+++ src/test/java/com/github/jcba/poimapper/XlsxWriterTest.java
@@ -14,7 +14,7 @@ import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class XlsxWriterJavaTest {
+class XlsxWriterTest {
 
     @Test
     void writesFields_whenAllAreAnnotated() {
